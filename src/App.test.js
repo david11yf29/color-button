f675452d0890356e5ab40338423e7d8083c24af3
david11yf29 <david11yf29@gmail.com>
2021-01-22 16:18:41 +0800

    updated

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,17 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  
+test('button has correct initial color', () => {
+  render(<App />);
+
+  // find an element with a role of button and text of 'Change to blue'
+  screen.getByRole('button', { name: "change to blue" });
+});
+
+test('button has correct initial text', () => {
+
+});
+
+test('button turns blue when clicked', () => {
+
 });
