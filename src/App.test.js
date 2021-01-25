20afeb6c4ad5cf2482b9e888ca8bb9e6509f5039
david11yf29 <david11yf29@gmail.com>
2021-01-25 16:14:45 +0800

    updated

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,6 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
+import { replaceCamelWithSpaces } from './App';
 
 test('button has correct initial color', () => {
   render(<App />);
@@ -43,3 +44,18 @@ test('Checkbox disables button on first click and enables on second click', () =
   fireEvent.click(checkbox);
   expect(colorButton).toBeEnabled();
 });
+
+describe('spaces before camel-case capital letters', () => {
+  test('Works for no inner capital letters', () => {
+    expect(replaceCamelWithSpaces("Red")).toBe("Red")
+  });
+
+  test('Works for one inner capital letter', () => {
+    expect(replaceCamelWithSpaces("MidnightBlue")).toBe("Midnight Blue")
+  }); 
+
+  test('Works for multiple inner capital letters', () => {
+    expect(replaceCamelWithSpaces("MediumVioletRed")).toBe("Medium Violet Red")
+  });
+
+}); 
